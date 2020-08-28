fe36c6149800d50a6f1c40a7cfccb2ced8b4c806
IamRash-7 <rasswanthshankar@rocketmail.com>
2020-08-28 15:10:48 +0530

    added lightbox

--- wildlife.js
+++ wildlife.js
@@ -37,3 +37,11 @@ toShow.forEach(ele=>{
 	observer.observe(ele)
 	console.log(ele)
 })
+
+//LightBox
+
+lightbox.option({
+      'resizeDuration': 200,
+      'wrapAround': true,
+      'disableScrolling':true
+    })
