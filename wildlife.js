0fda064397b1d92196194b3ef1eff7b74f018f41
IamRash-7 <rasswanthshankar@rocketmail.com>
2020-08-28 09:27:01 +0530

    edit wildlife

--- wildlife.js
+++ wildlife.js
@@ -1,3 +1,5 @@
+//Navbar colour change
+
 window.onscroll = function() {myFunction()};
 
 var header = document.getElementById("myHeader");
@@ -15,40 +17,23 @@ function myFunction(x) {
   }
 }
 
-var scroll = window.requestAnimationFrame ||
-            function(callback){ window.setTimeout(callback, 1000/60)};
+//Images Animation
 
-var elementsToShow = document.querySelectorAll('.show-on-scroll');
 
-function loop() {
+const toShow= document.querySelectorAll('.show-on-scroll');
 
-  elementsToShow.forEach(function (element) {
-    if (isElementInViewport(element)) {
-      element.classList.add('is-visible');
-    } else {
-      element.classList.remove('is-visible');
-    }
-  });
+observer= new IntersectionObserver((entries)=>{
+	entries.forEach(entry=>{
+		if(entry.intersectionRatio>0){
+			entry.target.classList.add("is-visible");
+		}
+		else{
+			entry.target.classList.remove('is-visible');
+		}
+	})
+})
 
-  scroll(loop);
-}
-
-loop();
-
-function isElementInViewport(el) {
-  // special bonus for those using jQuery
-  if (typeof jQuery === "function" && el instanceof jQuery) {
-    el = el[0];
-  }
-  var rect = el.getBoundingClientRect();
-  return (
-    (rect.top <= 0
-      && rect.bottom >= 0)
-    ||
-    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
-    ||
-    (rect.top >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
-  );
-}
+toShow.forEach(ele=>{
+	observer.observe(ele)
+	console.log(ele)
+})
